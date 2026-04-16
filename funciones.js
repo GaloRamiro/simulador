f2fa1eda2595caa4fa0015292ad9c6810d6ecebc
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-16 08:19:31 -0500

    Calcular disponible, funcion calcular

--- funciones.js
+++ funciones.js
@@ -1,1 +1,5 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos, egresos){
+     return Math.max(0,ingresos - egresos );
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,20 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+   let spIngresos = document.getElementById("txtIngresos");
+   let spEgresos = document.getElementById("txtEgresos");
+
+   let ingresos = spIngresos.value;
+   let egreso = spEgresos.value;
+
+   let floatingreso = parseFloat(ingresos);
+   let floatengreso = parseFloat(egreso);
+
+   let valor=calcularDisponible(floatingreso, floatengreso);
+
+   let spTexto = document.getElementById("spnDisponible"); 
+   spTexto.textContent=valor.toFixed(2);
+}
+
+
+
