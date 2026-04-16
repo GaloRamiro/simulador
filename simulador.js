31325aeb12a58ab41051eb741979c9c63ecc3cc3
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-16 12:09:52 -0500

    creacion de utils y optimizar codigo

--- simulador.js
+++ simulador.js
@@ -1,54 +1,36 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
 
 function calcular() {
-  let spIngresos = document.getElementById("txtIngresos");
-  let spEgresos = document.getElementById("txtEgresos");
+  let recuperarIngreso = recuperarFloat("txtIngresos");
+  let recuperarEgreso = recuperarFloat("txtEgresos");
+  let valor = calcularDisponible(recuperarIngreso, recuperarEgreso);
 
-  let ingresos = spIngresos.value;
-  let egreso = spEgresos.value;
-
-  let floatingreso = parseFloat(ingresos);
-  let floatengreso = parseFloat(egreso);
-
-  let valor = calcularDisponible(floatingreso, floatengreso);
-
-  let spDisponible = document.getElementById("spnDisponible");
-  spDisponible.textContent = valor.toFixed(2);
+  cambiarTexto("spnDisponible", valor);
 
   let CapacidadPago = calculaCapacidadPago(valor);
-  let spCapacidadPago = document.getElementById("spnCapacidadPago");
-  spCapacidadPago.textContent = CapacidadPago.toFixed(2);
 
-  let textoMonto = document.getElementById("txtMonto");
-  let textoplazo = document.getElementById("txtPlazo");
-  let textoTasaInteres = document.getElementById("txtTasaInteres");
+  cambiarTexto("spnCapacidadPago", CapacidadPago);
 
-  let monto = textoMonto.value;
-  let plazo = textoplazo.value;
-  let tasaInteres = textoTasaInteres.value;
-
-  let montoEntero = parseInt(monto);
-  let plazoEntero = parseInt(plazo);
-  let tasaIntereresEntero = parseInt(tasaInteres);
+  let recuperarMonto = recuperarEntero("txtMonto");
+  let recuperarPlazo = recuperarEntero("txtPlazo");
+  let recuperarInteres = recuperarEntero("txtTasaInteres");
 
   let valorInteres = calcularInteresSimple(
-    montoEntero,
-    tasaIntereresEntero,
-    plazoEntero,
+    recuperarMonto,
+    recuperarInteres,
+    recuperarPlazo,
   );
+  cambiarTexto("spnInteresPagar", valorInteres);
 
-  let spValor = document.getElementById("spnInteresPagar");
-  spValor.textContent = valorInteres.toFixed(2);
+  let totalPagar = calcularTotalPagar(recuperarMonto, valorInteres);
+  cambiarTexto("spnTotalPrestamo", totalPagar);
 
-  let totalPagar = calcularTotalPagar(montoEntero, valorInteres);
-  let spTotalValor = document.getElementById("spnTotalPrestamo");
-  spTotalValor.textContent = totalPagar.toFixed(2);
+  let cuotaMensual = calularCuotaMensual(totalPagar, recuperarPlazo);
 
-  let cuotaMensual = calularCuotaMensual(totalPagar, plazoEntero);
-  let spCuotaMensual = document.getElementById("spnCuotaMensual");
-  spCuotaMensual.textContent = cuotaMensual.toFixed(2);
+  cambiarTexto("spnCuotaMensual", cuotaMensual);
 
   let respuestaCredito = analizarCredito(capacidadPago, cuotaMensual);
+
   let spRepuesta = document.getElementById("spnEstadoCredito");
   if (respuestaCredito) {
     spRepuesta.textContent = "Credito Aprobado";
---
+++ utils.js
@@ -0,0 +1,23 @@
+function recuperarTexto(idCompontente){
+    let componente = document.getElementById(idCompontente);
+    let valor = componente.value;
+    return valor;
+}
+function recuperarFloat(idcomponten){
+  let valorTexto= recuperarTexto(idcomponten);
+  let valorfloat = parseFloat(valorTexto);
+  return valorfloat;
+  
+}
+
+function cambiarTexto(idTexto, valor){
+  let valorTexto = document.getElementById(idTexto);
+  valorTexto.textContent= valor.toFixed(2);
+}
+
+function recuperarEntero(idcomponten){
+  let valorTexto= recuperarTexto(idcomponten);
+  let valorEntero = parseInt(valorTexto);
+  return valorEntero;
+  
+}
