ef4928643189051f83b86b0a399348afedbed4d3
GaloRamiro <g_cisneros_23@hotmail.es>
2026-04-24 08:48:51 -0500

    Arreglos despues de examen

--- simulador.js
+++ simulador.js
@@ -7,8 +7,18 @@ function calcular() {
   }
 
   let recuperarIngreso = recuperarFloat("txtIngresos");
-  let recuperarEgreso = recuperarFloat("txtEgresos");
-  let valor = calcularDisponible(recuperarIngreso, recuperarEgreso);
+  //Valor recuperados examen
+  let recuperarArriendo = recuperarFloat("txtArriendo");
+  let recuperarAlimentacion = recuperarFloat("txtAlimentacion");
+  let recuperarVarios = recuperarFloat("txtVarios");
+
+  // Cierre Valores Examen
+
+  let gastos = recuperarArriendo + recuperarAlimentacion + recuperarVarios;
+
+  cambiarTexto("spnGastos", gastos);
+
+  let valor = calcularDisponible(recuperarIngreso, gastos);
 
   cambiarTexto("spnDisponible", valor);
 
@@ -47,20 +57,36 @@ function calcular() {
 function reiniciarFormulario() {
   // inputs
   let ingresos = document.getElementById("txtIngresos");
-  let egresos = document.getElementById("txtEgresos");
+  //let egresos = document.getElementById("txtEgresos");
+
+  //Examen
+  let arriendo = document.getElementById("txtArriendo");
+  let alimentacion = document.getElementById("txtAlimentacion");
+  let varios = document.getElementById("txtVarios");
+  // Examen
+
   let monto = document.getElementById("txtMonto");
   let plazo = document.getElementById("txtPlazo");
   let tasa = document.getElementById("txtTasaInteres");
   // limpiar
   ingresos.value = "";
-  egresos.value = "";
+  //egresos.value = "";
+  //Examen
+  arriendo.value = "";
+  alimentacion.value = "";
+  varios.value = "";
+  //Examen
   monto.value = "";
   plazo.value = "";
   tasa.value = "";
 
   // Limpiar mensajes
   ingresos.nextElementSibling.textContent = "";
-  egresos.nextElementSibling.textContent = "";
+  //EXAMEN
+  arriendo.nextElementSibling.textContent = "";
+  alimentacion.nextElementSibling.textContent = "";
+  varios.nextElementSibling.textContent = "";
+  //EXAMEN
   monto.nextElementSibling.textContent = "";
   plazo.nextElementSibling.textContent = "";
   tasa.nextElementSibling.textContent = "";
@@ -71,6 +97,9 @@ function reiniciarFormulario() {
   document.getElementById("spnInteresPagar").textContent = "";
   document.getElementById("spnTotalPrestamo").textContent = "";
   document.getElementById("spnCuotaMensual").textContent = "";
+
+  //Limpiar Gastos
+  document.getElementById("spnGastos").textContent = "";
 
   // Estado inicial
   document.getElementById("spnEstadoCredito").textContent = "ANALIZANDO...";
@@ -91,7 +120,16 @@ function validarInput(input) {
 
   let mensaje = "";
 
-  const esDecimal = ["txtIngresos", "txtEgresos"].includes(input.id);
+  //const esDecimal = ["txtIngresos", "txtEgresos"].includes(input.id);
+
+  //examen
+  const esDecimal = [
+    "txtIngresos",
+    "txtArriendo",
+    "txtAlimentacion",
+    "txtVarios",
+  ].includes(input.id);
+  //examen
 
   // 1. Vacío
   if (valor === "") {
@@ -139,7 +177,13 @@ function validarInput(input) {
 
 function validarFormulario() {
   const ingresos = document.getElementById("txtIngresos");
-  const egresos = document.getElementById("txtEgresos");
+
+  //examen
+  const arriendo = document.getElementById("txtArriendo");
+  const alimentacion = document.getElementById("txtAlimentacion");
+  const varios = document.getElementById("txtVarios");
+
+  //const egresos = document.getElementById("txtEgresos");
   const monto = document.getElementById("txtMonto");
   const plazo = document.getElementById("txtPlazo");
   const tasa = document.getElementById("txtTasaInteres");
@@ -149,14 +193,28 @@ function validarFormulario() {
   // Validaciones básicas
 
   if (!validarInput(ingresos)) valido = false;
-  if (!validarInput(egresos)) valido = false;
+  //if (!validarInput(egresos)) valido = false;
+  //Examen
+
+  if (!validarInput(arriendo)) valido = false;
+  if (!validarInput(alimentacion)) valido = false;
+  if (!validarInput(varios)) valido = false;
+
   if (!validarInput(monto)) valido = false;
   if (!validarInput(plazo)) valido = false;
   if (!validarInput(tasa)) valido = false;
 
   // Convertir valores
   const vIngresos = parseFloat(ingresos.value);
-  const vEgresos = parseFloat(egresos.value);
+  //const vEgresos = parseFloat(egresos.value);
+  //Examen
+
+  const vArriendo = parseFloat(arriendo.value);
+  const vAlimentacion = parseFloat(alimentacion.value);
+  const vVarios = parseFloat(varios.value);
+
+  const vEgresos = vArriendo + vAlimentacion + vVarios;
+
   const vMonto = parseInt(monto.value);
   const vPlazo = parseInt(plazo.value);
   const vTasa = parseInt(tasa.value);
@@ -166,10 +224,16 @@ function validarFormulario() {
     valido = false;
   }
 
+  //if (isNaN(vEgresos)) {
+  // mostrarError(egresos, "Ingrese un valor válido");
+  //valido = false;
+  //}
+  //EXAMEN
   if (isNaN(vEgresos)) {
-    mostrarError(egresos, "Ingrese un valor válido");
+    mostrarError(arriendo, "Ingrese valores válidos en gastos");
     valido = false;
   }
+  //EXAMEN
 
   // Reglas de negocio
   if (vIngresos < 100 || vIngresos > 99999) {
@@ -177,11 +241,21 @@ function validarFormulario() {
     valido = false;
   }
 
+  //if (vEgresos > vIngresos) {
+  // mostrarError(egresos, "Egresos no pueden ser mayores a ingresos");
+  //valido = false;
+  //}
+  //EXAMEN
   if (vEgresos > vIngresos) {
-    mostrarError(egresos, "Egresos no pueden ser mayores a ingresos");
+    mostrarError(arriendo, "Los gastos no pueden ser mayores a los ingresos");
+    mostrarError(
+      alimentacion,
+      "Los gastos no pueden ser mayores a los ingresos",
+    );
+    mostrarError(varios, "Los gastos no pueden ser mayores a los ingresos");
     valido = false;
   }
-
+  //EXAMEN
   if (vMonto < 500 || vMonto > 50000) {
     mostrarError(monto, "Monto entre 500 y 50000");
     valido = false;
@@ -204,4 +278,30 @@ function mostrarError(input, mensaje) {
   const errorMsg = input.nextElementSibling;
   input.classList.add("input-error");
   errorMsg.textContent = mensaje;
+}
+
+
+function actualizarGastosEnTiempoReal() {
+
+  let ingresos = recuperarFloat("txtIngresos");
+  let arriendo = recuperarFloat("txtArriendo");
+  let alimentacion = recuperarFloat("txtAlimentacion");
+  let varios = recuperarFloat("txtVarios");
+
+  // evitar NaN
+  if (isNaN(ingresos)) ingresos = 0;
+  if (isNaN(arriendo)) arriendo = 0;
+  if (isNaN(alimentacion)) alimentacion = 0;
+  if (isNaN(varios)) varios = 0;
+
+  let gastos = arriendo + alimentacion + varios;
+
+  cambiarTexto("spnGastos", gastos);
+
+  let disponible = calcularDisponible(ingresos, gastos);
+
+  cambiarTexto("spnDisponible", disponible);
+
+  let capacidad = calculaCapacidadPago(disponible);
+  cambiarTexto("spnCapacidadPago", capacidad);
 }
